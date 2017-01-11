09cff1ed88bbd75b8d860cc5539c0a0747985943
Ethan Thomson <ethan.thomson@pg.canterbury.ac.nz>
2017-01-11 15:29:11 -0800

    Updated to write params_val.py containing all required parameters in one text file

--- generate_parameters.py
+++ generate_parameters.py
@@ -1,74 +1,90 @@
 import numpy as np
+import sys
 
 # assume only two inputs (magnitude and epicentre)
 class earthquakeSource:
-    mag = 3.6
-    centroidDepth = 5 #in km +ve downwards
-    lon = 172.25
-    lat = -43.6
+    def __init__(self, mag, centroidDepth, lon, lat):
+        self.mag = float(mag)
+        self.centroidDepth = float(centroidDepth)  # in km +ve downwards
+        self.lon = float(lon)
+        self.lat = float(lat)
 
+    def __str__(self):
+        return "mag = %s centroidDepth = %s lon= %s lat = %s" % (self.mag, self.centroidDepth, self.lon, self.lat)
 
-# generate the domain extents based on the magnitude and epicentre
-
-rawXYextent =  np.power(10 , ( 0.55 * earthquakeSource.mag - 1.2 ))
-XYextent = round(2*rawXYextent,0)
-rawZextent = 10 + earthquakeSource.centroidDepth + (10 * np.power((0.5*rawXYextent/earthquakeSource.centroidDepth),(0.3)))
-Zextent = round(rawZextent,0)
-timeExponent = max(1,0.5*earthquakeSource.mag - 1)
-tmax = np.power(10, timeExponent)
 
 class Domain:
-#    MODEL_VERSION = '1.65_NZ'
-    MODEL_VERSION = '1.02'
-    MIN_VS = 0.5
-    TOPO_TYPE = 'BULLDOZED'
-    EXTENT_Z_SPACING = 0.1
-    EXTENT_LATLON_SPACING = 0.1
-    OUTPUT_DIR = 'Rapid_Model'
-    EXTENT_ZMIN = 0
-    ORIGIN_ROT = 0
-    EXTRACTED_SLICE_PARAMETERS_DIRECTORY = 'SliceParametersNZ'
+    def __init__(self, eq_src, output_dir, slice_params_dir, model_ver='1.65_NZ', min_vs=0.5, topo_type='BULLDOZED',
+                 extent_z_spac=0.1, extent_latlon_spac=0.1, extent_zmin=0, origin_rot=0, code='rt'):
+        self.MODEL_VERSION = model_ver
+        self.MIN_VS = min_vs
+        self.TOPO_TYPE = topo_type
+        self.EXTENT_Z_SPACING = extent_z_spac
+        self.EXTENT_LATLON_SPACING = extent_latlon_spac
+        self.OUTPUT_DIR = output_dir
+        self.EXTENT_ZMIN = extent_zmin
+        self.ORIGIN_ROT = origin_rot
+        self.EXTRACTED_SLICE_PARAMETERS_DIRECTORY = slice_params_dir
+        self.CODE = code
 
-    ORIGIN_LAT = earthquakeSource.lat
-    ORIGIN_LON = earthquakeSource.lon
-    EXTENT_X = XYextent
-    EXTENT_Y = XYextent
-    EXTENT_ZMAX = Zextent
-    T_MAX = tmax
+        self.ORIGIN_LAT = eq_src.lat
+        self.ORIGIN_LON = eq_src.lon
 
-fileName = 'Rapid_Velocity_Model_Parameters_Full.txt'
-# write domain parameters to a shell script file that will call the velocity model
-fid = open(fileName, 'w')
-fid.write('MODEL_VERSION={0}\n'.format(Domain.MODEL_VERSION))
-fid.write('OUTPUT_DIR={0}\n'.format(Domain.OUTPUT_DIR))
-fid.write('ORIGIN_LAT={0}\n'.format(Domain.ORIGIN_LAT))
-fid.write('ORIGIN_LON={0}\n'.format(Domain.ORIGIN_LON))
-fid.write('ORIGIN_ROT={0}\n'.format(Domain.ORIGIN_ROT))
-fid.write('EXTENT_X={0}\n'.format(Domain.EXTENT_X))
-fid.write('EXTENT_Y={0}\n'.format(Domain.EXTENT_Y))
-fid.write('EXTENT_ZMAX={0}\n'.format(Domain.EXTENT_ZMAX))
-fid.write('EXTENT_ZMIN={0}\n'.format(Domain.EXTENT_ZMIN))
-fid.write('EXTENT_Z_SPACING={0}\n'.format(Domain.EXTENT_Z_SPACING))
-fid.write('EXTENT_LATLON_SPACING={0}\n'.format(Domain.EXTENT_LATLON_SPACING))
-fid.write('MIN_VS={0}\n'.format(Domain.MIN_VS))
-fid.write('TOPO_TYPE={0}\n'.format(Domain.TOPO_TYPE))
-fid.close()
+        # generate the domain extents based on the magnitude and epicentre
 
-fileName = 'Rapid_Velocity_Model_Parameters_For_Domain_Plotting.txt'
-# write domain parameters to a shell script file that will call the velocity model
-fid = open(fileName, 'w')
-fid.write('ORIGIN_LAT={0}\n'.format(Domain.ORIGIN_LAT))
-fid.write('ORIGIN_LON={0}\n'.format(Domain.ORIGIN_LON))
-fid.write('ORIGIN_ROT={0}\n'.format(Domain.ORIGIN_ROT))
-fid.write('EXTENT_X={0}\n'.format(Domain.EXTENT_X))
-fid.write('EXTENT_Y={0}\n'.format(Domain.EXTENT_Y))
-fid.write('EXTENT_LATLON_SPACING={0}\n'.format(Domain.EXTENT_LATLON_SPACING))
-fid.close()
+        rawXYextent = np.power(10, (0.55 * eq_src.mag - 1.2))
+        XYextent = round(2 * rawXYextent, 0)
+        rawZextent = 10 + eq_src.centroidDepth + (10 * np.power((0.5 * rawXYextent / eq_src.centroidDepth), (0.3)))
+        Zextent = round(rawZextent, 0)
+        timeExponent = max(1, 0.5 * eq_src.mag - 1)
+        tmax = np.power(10, timeExponent)
 
-fileName = 'Simulation_Duration.txt'
-# write domain parameters to a shell script file that will call the velocity model
-fid = open(fileName, 'w')
-fid.write('T_MAX={0}\n'.format(Domain.T_MAX))
-fid.close()
+        self.EXTENT_X = XYextent
+        self.EXTENT_Y = XYextent
+        self.EXTENT_ZMAX = Zextent
+        self.T_MAX = round(tmax,0)
+
+    def write(self):
+        with open('params_vel.py', 'w') as fid:
+            fid.write("MODEL_VERSION = '{0}'\n".format(self.MODEL_VERSION))
+            fid.write("OUTPUT_DIR = '{0}'\n".format(self.OUTPUT_DIR))
+            fid.write("ORIGIN_LAT = '{0}'\n".format(self.ORIGIN_LAT))
+            fid.write("ORIGIN_LON = '{0}'\n".format(self.ORIGIN_LON))
+            fid.write("ORIGIN_ROT = '{0}'\n".format(self.ORIGIN_ROT))
+            fid.write("EXTENT_X = '{0}'\n".format(self.EXTENT_X))
+            fid.write("EXTENT_Y = '{0}'\n".format(self.EXTENT_Y))
+            fid.write("EXTENT_ZMAX = '{0}'\n".format(self.EXTENT_ZMAX))
+            fid.write("EXTENT_ZMIN = '{0}'\n".format(self.EXTENT_ZMIN))
+            fid.write("EXTENT_Z_SPACING = '{0}'\n".format(self.EXTENT_LATLON_SPACING))
+            fid.write("EXTENT_LATLON_SPACING = '{0}'\n".format(self.EXTENT_LATLON_SPACING))
+            fid.write("MIN_VS = '{0}'\n".format(self.MIN_VS))
+            fid.write("TOPO_TYPE= '{0}'\n\n".format(self.TOPO_TYPE))
+
+            fid.write("CODE = '{0}'\n".format(self.CODE))
+            fid.write("HH = '{0:1.3f}'\n".format(self.EXTENT_LATLON_SPACING))
+            fid.write("NX = '{0:4.0f}'\n".format(2*self.EXTENT_X / self.EXTENT_LATLON_SPACING))  # EXTENT_X/HH
+            fid.write("NY = '{0:4.0f}'\n".format(2*self.EXTENT_Y / self.EXTENT_LATLON_SPACING))
+            fid.write("NZ = '{0:3.0f}'\n".format(self.EXTENT_ZMAX / self.EXTENT_LATLON_SPACING))
+            fid.write("SIM_DURATION = '{0}'\n".format(self.T_MAX))
+            fid.write("SUFX = '_{0}01-h{1:1.3f}'\n".format(self.CODE, self.EXTENT_LATLON_SPACING))
+            fid.close()
+
+
+if len(sys.argv) != 5:
+    print "Usage %s mag centroidDepth lon lat" % sys.argv[0]
+    sys.exit()
+
+mag, centroidDepth, lon, lat, = sys.argv[1:]
+eq_src = earthquakeSource(mag, centroidDepth, lon, lat)
+print eq_src
+output_dir = "Rapid_Model"
+slice_params_dir = "SliceParametersNZ"
+domain = Domain(eq_src, output_dir, slice_params_dir)
+domain.write()
+
+
+
+
+
 
 

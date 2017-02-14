a658398fb9fe0fa00831166876cfd4111e8244d4
thomsonem <ethan.thomson@outlook.com>
2017-02-14 11:28:55 -0800

    Modified clean.py

--- clean.py
+++ clean.py
@@ -1,17 +1,15 @@
 # load in libraries
 from subprocess import call
 
-
-
 call(['rm', '-rf', 'Rapid_Model']) 
 call(['rm', '-rf', 'Velocity-Model/Rapid_Model'])
 call(['rm', '-rf', 'Domain'])
 
-call(['mv', '-rf', 'GMT/Plots'])
+call(['rm', '-rf', 'GMT/Plots'])
 call(['rm', '-rf','gmt.conf'])
 call(['rm', '-rf','gmt.history'])
 
-call(['rm', 'extractVeloModel.sh'])
-call(['rm', 'generateVeloModel.sh'])
+call(['rm', '-rf', 'extractVeloModel.sh'])
+call(['rm', '-rf', 'generateVeloModel.sh'])
 call(['rm', '-rf','GMT/Cross_Sections/'])
 call(['rm', '-rf','Velocity-Model/SliceParametersNZ'])
